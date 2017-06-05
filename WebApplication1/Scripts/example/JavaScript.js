56762bcfd56c71e33b2d8589ab44044bb765bddc
Anatol <Anatol@DESKTOP-4CDJ629>
2017-06-05 11:19:25 +0200

    Add project files.

---
+++ WebApplication1/Scripts/example/JavaScript.js
@@ -0,0 +1,6 @@
+﻿function SayHello(user) {
+    alert("Hello, " + user);
+}
+
+var user = "Jan Nowak";
+SayHello(user);
---
+++ WebApplication1/Scripts/example/TypeScript.ts
@@ -0,0 +1,20 @@
+﻿class User {
+    private name: String;
+    private surname: String;
+    constructor(name: String, surname: String) {
+        this.name = name;
+        this.surname = surname;
+    };
+    public GetFullName(): String {
+        return (this.name + " " +  this.surname);
+    }
+}
+
+function SayHello(user: User): void {
+    alert("Hello, " + user.GetFullName());
+}
+
+$(document).ready(() => {
+    var user: User = new User("Jan", "Nowak");
+    SayHello(user);
+});
